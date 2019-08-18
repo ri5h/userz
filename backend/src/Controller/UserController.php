62ba432671d8220d67e3f1a9a905c4ffaf50177e
Rishiraj Purohit <purohitji007@gmail.com>
2019-08-18 18:36:54 +0530

    Add User get and post routes

--- backend/src/Controller/UserController.php
+++ backend/src/Controller/UserController.php
@@ -2,19 +2,53 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
 
-class UserController extends AbstractController
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+
+/**
+ * @Route("/api",name="user_api")
+ */
+class UserController extends FOSRestController
 {
     /**
-     * @Route("/user", name="user")
+     * @Rest\Get("/user")
+     * 
+     * @return Response
      */
-    public function index()
+    public function readAll(UserRepository $userRepository)
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/UserController.php',
-        ]);
+        //get all users from database, return as json
+        $users = $userRepository->findAll();
+        return $this->handleView($this->view($users));
+    }
+
+    /**
+     * @Rest\Post("/user")
+     * 
+     * @return Response
+     */
+    public function addUser(Request $request, EntityManagerInterface $entityManagerInterface)
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $data = json_decode($request->getContent($request), true);
+        
+        $form->submit($data);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $entityManagerInterface->persist($user);
+            $entityManagerInterface->flush();
+            return $this->handleView($this->view(['status'=>'ok'], Response::HTTP_CREATED));
+        }        
+        return $this->handleView($this->view($form->getErrors()));
     }
 }
