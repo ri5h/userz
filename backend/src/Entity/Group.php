75ceb6d2e95fd572f6d99c60caf2be258edac7c7
Rishiraj Purohit <purohitji007@gmail.com>
2019-08-18 18:42:57 +0530

    Add quotes to escape reserved keyword

--- backend/src/Entity/Group.php
+++ backend/src/Entity/Group.php
@@ -5,9 +5,10 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-
+use Doctrine\ORM\Mapping\Table;
 /**
  * @ORM\Entity(repositoryClass="App\Repository\GroupRepository")
+ * @Table(name="`group`")
  */
 class Group
 {
